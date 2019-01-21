260b7b1c821b416a113c60fdd2af3b48ef1c55d1
Chaimite <adrianocorreiacesar@gmail.com>
2019-01-21 20:26:15 +0000

    <3.6 >

--- rango/urls.py
+++ rango/urls.py
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from rango import views
-from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^admin/', include('rango.urls')),
-    #above maps anys URLs starting
-    #with rango/ to be handled by
-    #the rango application
-    url(r'^admin/' , admin.site.urls),
 ]
