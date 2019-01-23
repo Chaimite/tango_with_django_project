971b085b7eb9e94b14b74aa5d2705df77f99424f
Chaimite <adrianocorreiacesar@gmail.com>
2019-01-23 10:11:15 +0000

    <creating template folder>

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,4 @@ def index(request):
 def about(request):
     return HttpResponse("The person that is inserting this message asked the tutors where the html had " \
                                    "to be inserted. The answer given was you have to do it yourself. What does the word where have to" \
-                                   "to do with do it yourself?")
+                                   "do with do it yourself?")
